91f6996b554f1f73ec45420b5bd4562fcb2b792f
Josh Enders <josh.enders@gmail.com>
2014-12-22 16:48:36 -0800

    Updates mnt replacement message in envrionment.sh

--- lib/environment.sh
+++ lib/environment.sh
@@ -1,7 +1,7 @@
 # security/sanity
 export PATH="/usr/local/lib/logpipe:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-mnt="YOUR_FTP_DELIVERY_PATH"
+mnt="YOUR_DELIVERY_PATH"
 
 stage1="${mnt}/stage1-incoming"
 stage2="${mnt}/stage2-holding"
